7fc97633fda21a5b07b86948b05d134a945ed426
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-03-07 22:13:55 +0530

    start project

---
+++ StartingProject/StartingProject/LitttleLemonLogo.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
+
+
